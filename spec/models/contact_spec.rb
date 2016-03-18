a7b12712dc0c029119708674e72cad5e368495c4
Abhilash A K <abhilashamballur@gmail.com>
2016-03-18 10:32:32 +0000

    initial commit

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'shoulda/matchers'
+
+RSpec.describe Contact, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a factory" do
+    expect(FactoryGirl.build(:contact)).to be_valid
+  end
+
+  context "validations" do
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:title) }
+  end
+end
